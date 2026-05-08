19a073519a8e04361443711a9dca9da43e6b63fc
Varun-006 <varund9110820617@gmail.com>
2026-05-08 16:08:52 +0530

    PROGRAM5

---
+++ Full-Stack Web Development/ISSUE_TRACKER/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import IssueList from './issue.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <IssueList />
+    </div>
+  );
+}
+export default App;
